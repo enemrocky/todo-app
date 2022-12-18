1c0e8deec53f79cdd709f5fc41ec3a3e90f2d453
Enem <craigcharltonlunga1000@gmail.com>
2022-12-18 11:34:51 -0800

    added js functions commit

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-const outputMessage = document.querySelector('#message');
-const deleteBtn = document.querySelector('#delete');
-const editBtn = document.querySelector('#edit');
-const inputGroup = document.querySelector('#input-group');
-const input = document.querySelector('#input');
-const addBtn = document.querySelector('#add-btn');
-const addTaskBtn = document.querySelector('#add-task-btn');
+const outputMessage = document.querySelector("#message");
+const deleteBtn = document.querySelector("#delete");
+const editBtn = document.querySelector("#edit");
+const inputGroup = document.querySelector("#input-group");
+const input = document.querySelector("#input");
+const addBtn = document.querySelector("#add-btn");
+const addTaskBtn = document.querySelector("#add-task-btn");
+const todoApp = document.querySelector("#todo-app");
+const newTask = document.querySelector("#task");
 
-
-addTaskBtn.addEventListener("click", function(){
-   if(inputGroup.style.display = 'none'){
-    inputGroup.style.display = 'flex';
-   }
+addTaskBtn.addEventListener("click", function () {
+  if ((inputGroup.style.display = "none")) {
+    inputGroup.style.display = "flex";
+  }
 });
 
-addBtn.addEventListener('click', function(){
-
+addBtn.addEventListener("click", function () {
+    todoApp.append(newTask);
+    outputMessage.innerText = input.value;
 });
+
+deleteBtn.addEventListener("click", function(){
+    // deleteBtn.preventDefault();
+    task.remove();
+})
