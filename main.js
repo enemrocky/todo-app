9e077d54fcb70fcf00cbf7461e2dddf48e9d9ed8
Enem <craigcharltonlunga1000@gmail.com>
2022-12-18 11:19:38 -0800

    initial js commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const outputMessage = document.querySelector('#message');
+const deleteBtn = document.querySelector('#delete');
+const editBtn = document.querySelector('#edit');
+const inputGroup = document.querySelector('#input-group');
+const input = document.querySelector('#input');
+const addBtn = document.querySelector('#add-btn');
+const addTaskBtn = document.querySelector('#add-task-btn');
+
+
+addTaskBtn.addEventListener("click", function(){
+   if(inputGroup.style.display = 'none'){
+    inputGroup.style.display = 'flex';
+   }
+});
+
+addBtn.addEventListener('click', function(){
+
+});
