616c897c8939bbe3c9b2f5e25930583b00e80a44
Enem <craigcharltonlunga1000@gmail.com>
2022-12-19 01:18:56 -0800

    first commit

--- main.js
+++ main.js
@@ -1,25 +1,55 @@
-const outputMessage = document.querySelector("#message");
-const deleteBtn = document.querySelector("#delete");
-const editBtn = document.querySelector("#edit");
-const inputGroup = document.querySelector("#input-group");
-const input = document.querySelector("#input");
-const addBtn = document.querySelector("#add-btn");
-const addTaskBtn = document.querySelector("#add-task-btn");
-const todoApp = document.querySelector("#todo-app");
-const newTask = document.querySelector("#task");
+let input = document.querySelector('#input');
+let list = document.querySelector('#list');
+let errorText = document.querySelector('#error-text');
+let i = 1;
 
-addTaskBtn.addEventListener("click", function () {
-  if ((inputGroup.style.display = "none")) {
-    inputGroup.style.display = "flex";
-  }
-});
+function filterText(text) {
+    if(text) {
+        if(text.length > 3) {
+            return text;
+        } else {
+            errorText.innerHTML = 'Type at least a word.';
+            return false;
+        }
+    }
+    errorText.innerHTML = 'Input cannot be empty.';
+    return false;
+}
 
-addBtn.addEventListener("click", function () {
-    todoApp.append(newTask);
-    outputMessage.innerText = input.value;
-});
+function showList(input) {
+    let today = new Date();
+    let date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+    let time = today.getHours() + ':' + today.getMinutes()  + ':' + today.getSeconds();
+    let dateTime = date + ' | ' + time;
+    list.innerHTML += 
+        `<li class="list-item" id="list${i}">
+            <div class="list-item-wrapper">
+                <div class="list-text">
+                    <span id="list-text${i}">${input}</span>
+                    <p id="date-text">${dateTime}</p>
+                </div>
+                <div id="action-btn">
+                    <i class="" onclick="deleteList(${i})">
+                        <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                    </i>
+                </div>
+            </div>
+        </li>`;
+    i++;
+}
 
-deleteBtn.addEventListener("click", function(){
-    // deleteBtn.preventDefault();
-    task.remove();
-})
+function addList() {
+    let inputText = input.value;
+
+    if(filterText(inputText)) {
+        showList(filterText(inputText));
+
+        input.value = '';
+        errorText.innerHTML = '';
+    }
+}
+
+function deleteList(id) {
+    let deleteList = document.getElementById(`list${id}`);
+    list.removeChild(deleteList);
+}
