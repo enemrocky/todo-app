ba32e02989010c050bf4541fbc2202d62b8c5b8a
Craig Charlton <craigcharltonlunga1000@gmail.com>
2022-12-24 04:51:19 -0800

    added functionality

--- main.js
+++ main.js
@@ -2,4 +2,18 @@ const input = document.querySelector('#todoInfo');
 const addBtn = document.querySelector('#add-btn');
 const listContainer = document.querySelector('#todoList');
 
-addBtn.addEventListener
+addBtn.addEventListener('click', function(){
+    const listItem = document.createElement('p');
+    listItem.innerHTML = `<div class="d-flex justify-content-between">${input.value}  <i class="bi bi-trash ms-5"></i></div>`;
+    listContainer.append(listItem);
+    input.value = '';
+    const deleteBtn = document.querySelector('.delete')
+    listItem.addEventListener('click', function(){
+        listItem.style.textDecoration = 'line-through';
+    })
+    deleteBtn.addEventListener('click', function(){
+        listItem.remove();
+    })
+
+})
+
