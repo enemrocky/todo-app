8aa9a7ea90059ad0cdd542ab46bae4e52980deb4
Craig Charlton <craigcharltonlunga1000@gmail.com>
2022-12-28 00:51:06 -0800

    added key and fixed input-group alignment issues

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const input = document.querySelector('#todoInfo');
 const addBtn = document.querySelector('#add-btn');
 const listContainer = document.querySelector('#todolist');
+const key = document.querySelector('.key');
+
 
 listContainer.style.width = '100%';
 listContainer.style.padding = '0 auto';
+
+const keyItem = document.createElement('p');
+const keyItem2 = document.createElement('p');
+keyItem.innerText = 'click ToDo to line through'
+keyItem2.innerText = 'double click ToDo to delete from list'
+key.append(keyItem,keyItem2)
+
 
 addBtn.addEventListener('click', function(){
     const listItem = document.createElement('p');
